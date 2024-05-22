fe3cb950475abed38e0807bdad3aa28419203c20
IlyshaThePapo <ilyaogur0@gmail.com>
2024-05-22 23:50:34 +0300

    Add files via upload

---
+++ NumberHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace SearchForNumbers1
+{
+    public class NumberHelper
+    {
+        public static List<int> FindNumbers(string inputText)
+        {
+            List<int> numbers = new List<int>();
+
+            string pattern = @"\b\d+\b"; // Регулярное выражение для поиска чисел
+            MatchCollection matches = Regex.Matches(inputText, pattern);
+
+            foreach (Match match in matches)
+            {
+                numbers.Add(int.Parse(match.Value));
+            }
+
+            return numbers;
+        }
+    }
+}
